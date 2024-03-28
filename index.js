e9c48ae21a3a56859cf97947a1e2f727700bcb3f
mageshkrishna <gmageshkrishna2003@gmail.com>
2024-03-28 15:31:58 +0530

    first commit2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./Controller/loginrouter');
-
+let cors = require('cors');
 
 
 const app = express();
@@ -14,10 +14,25 @@ app.use(bodyParser.json());
 
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/qlozet").then(() => {
-  console.log("Connected to DB");
-});
-
+app.use(
+    cors({
+      origin: "*",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true, 
+      optionsSuccessStatus: 204, 
+    })
+  );
+  const uri = "mongodb+srv://hornedfoe:Ranjith123@cluster0.ngirblx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+  mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to MongoDB Atlas");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+      process.exit(1);
+    });
+  
 app.use('/auth',router);
 
 app.listen(PORT, () => {
